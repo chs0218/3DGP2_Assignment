17f09c66f406543fe61254a7931ed88ecc44eeed
Choi Hae Seong <2018180044@tukorea.ac.kr>
2022-10-19 21:26:00 +0900

    Revert "기하셰이더 수정"
    This reverts commit 7e9046262ed2ad5929c9c9fc665f2042708a3daa.

--- 3DGP2_Assignment00/BillBoardGeometryShader.hlsl
+++ 3DGP2_Assignment00/BillBoardGeometryShader.hlsl
@@ -1,48 +1,20 @@
-struct MATERIAL
-{
-	float4 m_cAmbient;
-	float4 m_cDiffuse;
-	float4 m_cSpecular; //a = power
-	float4 m_cEmissive;
-};
-
-cbuffer cbGameObjectInfo : register(b0)
-{
-	matrix gmtxGameObject : packoffset(c0);
-	MATERIAL gMaterial : packoffset(c4);
-	uint gnTexturesMask : packoffset(c8);
-}
-
-cbuffer cbCameraInfo : register(b1)
-{
-	matrix gmtxView : packoffset(c0);
-	matrix gmtxProjection : packoffset(c4);
-	float3 gvCameraPosition : packoffset(c8);
-}
-
 struct VS_OUTPUT
 {
-	float3 position : POSITION;
-	float2 size : TEXCOORD;
-};
-
-struct GS_OUT
-{
 	float4 position : SV_POSITION;
-	float2 size : TEXCOORD;
+	float2 uv : TEXCOORD;
 };
 
 [maxvertexcount(3)]
 void GS_BillBoard(
 	triangle VS_OUTPUT input[3], 
-	inout TriangleStream< GS_OUT > outStream
+	inout TriangleStream< VS_OUTPUT > outStream
 )
 {
-	GS_OUT output;
+	VS_OUTPUT output;
 	for (int i = 0; i < 3; ++i)
 	{
-		output.position = mul(mul(mul(float4(input[i].position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
-		output.size = input[i].size;
+		output.position = input[i].position;
+		output.uv = input[i].uv;
 		outStream.Append(output);
 	}
 }
--- 3DGP2_Assignment00/BillBoardVertexShader.hlsl
+++ 3DGP2_Assignment00/BillBoardVertexShader.hlsl
@@ -1,19 +1,41 @@
+struct MATERIAL
+{
+	float4 m_cAmbient;
+	float4 m_cDiffuse;
+	float4 m_cSpecular;
+	float4 m_cEmissive;
+};
+
+cbuffer cbGameObjectInfo : register(b0)
+{
+	matrix gmtxGameObject : packoffset(c0);
+	MATERIAL gMaterial : packoffset(c4);
+	uint gnTexturesMask : packoffset(c8);
+}
+
+cbuffer cbCameraInfo : register(b1)
+{
+	matrix gmtxView : packoffset(c0);
+	matrix gmtxProjection : packoffset(c4);
+	float3 gvCameraPosition : packoffset(c8);
+}
+
 struct VS_INPUT
 {
 	float3 position : POSITION;
-	float2 size : TEXCOORD;
+	float2 uv : TEXCOORD;
 };
 
 struct VS_OUTPUT
 {
-	float3 position : POSITION;
-	float2 size : TEXCOORD;
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
 };
 
 VS_OUTPUT VS_BillBoard(VS_INPUT input)
 {
 	VS_OUTPUT output;
-	output.position = input.position;
-	output.size = input.size;
+	output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+	output.uv = input.uv;
 	return(output);
 }
